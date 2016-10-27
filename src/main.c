c7179ed68ee133c43783cb88e3abe9e4ebd6b3e8
Yannic Staudt <yannic@staudt-engineering.com>
2016-10-27 07:32:31 +0200

    Preparing libopencm3 project / cpu clock initialization at 72MHz using HSE

---
+++ src/main.c
@@ -0,0 +1,31 @@
+/**
+	Open firmware for Waveshare 7" capacitive touchscreen 
+	Copyright (C) 2016 Yannic Staudt / Staudt Engineering
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+ */	
+
+#include <stdlib.h>
+#include <libopencm3/gd32/rcc.h>
+
+int main(void)
+{
+	// setup the CPU
+	rcc_clock_setup_in_hse_8mhz_out_72mhz();    // 72MHz clock using on-board crystal / HSE
+
+
+	 while (1) {
+	 }
+}
